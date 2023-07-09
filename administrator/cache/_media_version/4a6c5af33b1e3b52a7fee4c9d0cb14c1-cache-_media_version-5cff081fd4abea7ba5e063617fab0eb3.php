1d93ed73a542e2e2e6045c5e47a8c0f0c4ed65e3
owaisorakzai <84202122+owaisorakzai@users.noreply.github.com>
2023-07-09 13:10:07 +0500

    first

---
+++ administrator/cache/_media_version/4a6c5af33b1e3b52a7fee4c9d0cb14c1-cache-_media_version-5cff081fd4abea7ba5e063617fab0eb3.php
@@ -0,0 +1,1 @@
+<?php die("Access Denied"); ?>#x#a:2:{s:6:"result";s:32:"7d1dd40c2006998c5f6765d0c4cb8b9a";s:6:"output";s:0:"";}
---
+++ templates/rentotemplate/index.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>rento</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>      <!-- style css -->
+      <link rel="stylesheet" href="templates/rental/css/style.css">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="templates/rental/css/responsive.css">
+      <!-- fevicon -->
+      <link rel="icon" href="templates/rental/images/fevicon.png" type="image/gif" />
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="templates/rental/css/jquery.mCustomScrollbar.min.css">
+      <!-- Tweaks for older IEs-->
+      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+   </head>
+   <!-- body -->
+   <body class="main-layout">
+      <!-- loader  -->
+      <div class="loader_bg">
+         <div class="loader"><
+           src="templates/rental/images/loading.gif" alt="#" /></div>
+      </div>
+      <!-- end loader -->
+      <!-- header -->
+      <header>
+         <div class="header">
+            <div class="container">
+               <div class="row">
+                  <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                     <div class="full">
+                        <div class="center-desk">
+                           <div class="logo">
+                              <a href="index.html"><img src="templates/rental/images/logo.png" alt="#" /></a>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+                     <nav class="navigation navbar navbar-expand-md navbar-dark ">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarsExample04">
+                           <ul class="navbar-nav mr-auto">
+                              <li class="nav-item">
+                                 <a class="nav-link" href="http://localhost/joomla3/index.php/home-page"> Home  </a>
+                              </li>
+                              <li class="nav-item">
+                                 <a class="nav-link" href="http://localhost/joomla3/index.php/book">Book Now</a>
+                              </li>
+                              <li class="nav-item">
+                                 <a class="nav-link" href="http://localhost/joomla3/index.php/about">Contact us</a>
+                              </li>
+                           </ul>
+                        </div>
+                     </nav>
+                  </div>
+               </div>
+            </div>
+      </header>
+      <!-- end header inner -->
+      <!-- end header -->
+      <!-- banner -->
+      <section class="banner_main">
+         <div class="container">
+            <div class="row d_flex">
+               <div class="col-md-12">
+                  <div class="text-bg">
+                     <h1>Find best car Here For rent</h1>
+                     <strong>Best Car Company</strong>
+                     <span>2022</span>
+                     <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since theLorem Ipsum is simply dummy text of the printing 
+                        <head></head>
+                     </p>
+                     <a href="http://localhost/joomla3/index.php/book">Book</a>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      </div>
+      <!-- end banner -->
+      <!-- car -->
+      <div  class="car">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="titlepage">
+                     <h2>VARIETY OF CARS </h2>
+                     <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna</span>
+                  </div>
+               </div>
+            </div>
+            <div class="row">
+               <div class="col-md-4 padding_leri">
+                  <div class="car_box">
+                     <figure><img src="templates/rental/images/car_img1.png" alt="#"/></figure>
+                     <h3>Hundai</h3>
+                  </div>
+               </div>
+               <div class="col-md-4 padding_leri">
+                  <div class="car_box">
+                     <figure><img src="templates/rental/images/car_img2.png" alt="#"/></figure>
+                     <h3>Audi</h3>
+                  </div>
+               </div>
+               <div class="col-md-4 padding_leri">
+                  <div class="car_box">
+                     <figure><img src="templates/rental/images/car_img3.png" alt="#"/></figure>
+                     <h3>Bmw x5</h3>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- end car -->
+      <!-- cutomer -->
+      <div class="cutomer">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="titlepage">
+                     <h2>What is says our cutomer</h2>
+                  </div>
+               </div>
+            </div>
+            <div class="row">
+               <div class="col-md-12">
+                  <div id="myCarousel" class="carousel slide cutomer_Carousel " data-ride="carousel">
+                     <ol class="carousel-indicators">
+                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#myCarousel" data-slide-to="1"></li>
+                        <li data-target="#myCarousel" data-slide-to="2"></li>
+                     </ol>
+                     <div class="carousel-inner">
+                        <div class="carousel-item active">
+                           <div class="container">
+                              <div class="carousel-caption ">
+                                 <div class="cross_img">
+                                    <figure><img src="templates/rental/images/cross_img.png" alt="#"/></figure>
+                                 </div>
+                                 <div class="our cross_layout">
+                                    <div class="test_box">
+                                       <h4>Due markes</h4>
+                                       <span>Rental</span>
+                                       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit ess</p>
+                                       <i><img src="templates/rental/images/te1.png" alt="#"/></i>
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                        <div class="carousel-item">
+                           <div class="container">
+                              <div class="carousel-caption">
+                                 <div class="cross_img">
+                                    <figure><img src="templates/rental/images/cross_img.png" alt="#"/></figure>
+                                 </div>
+                                 <div class="our cross_layout">
+                                    <div class="test_box">
+                                       <h4>Due markes</h4>
+                                       <span>Rental</span>
+                                       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit ess</p>
+                                       <i><img src="templates/rental/images/te1.png" alt="#"/></i>
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                        <div class="carousel-item">
+                           <div class="container">
+                              <div class="carousel-caption">
+                                 <div class="cross_img">
+                                    <figure><img src="templates/rental/images/cross_img.png" alt="#"/></figure>
+                                 </div>
+                                 <div class="our cross_layout">
+                                    <div class="test_box">
+                                       <h4>Due markes</h4>
+                                       <span>Rental</span>
+                                       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit ess</p>
+                                       <i><img src="templates/rental/images/te1.png" alt="#"/></i>
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                     <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                     <span class="sr-only">Previous</span>
+                     </a>
+                     <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                     <span class="sr-only">Next</span>
+                     </a>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- end cutomer -->
+      <!--  footer -->
+      <footer>
+         <div class="footer">
+            <div class="container">
+               <div class="row">
+                  <div class="col-md-12">
+                     <div class="cont_call">
+                        <h3> <strong class="multi color_chang"> Call Now</strong><br>
+                           (+1) 12345667890
+                        </h3>
+                     </div>
+                     <div class="cont">
+                        <h3> <strong class="multi"> Rento</strong> 
+                           Booking Open
+                        </h3>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="copyright">
+               <div class="container">
+                  <div class="row">
+                     <div class="col-md-12">
+                        <p>© 2019 All Rights Reserved.
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </footer>
+      <!-- end footer -->
+      <!-- Javascript files-->
+      <script src="templates/rental/js/jquery.min.js"></script>
+      <script src="templates/rental/js/popper.min.js"></script>
+      <script src="templates/rental/js/bootstrap.bundle.min.js"></script>
+      <script src="templates/rental/js/jquery-3.0.0.min.js"></script>
+      <script src="templates/rental/js/plugin.js"></script>
+      <!-- sidebar -->
+      <script src="templates/rental/js/jquery.mCustomScrollbar.concat.min.js"></script>
+      <script src="templates/rental/js/custom.js"></script>
+      <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+   </body>
